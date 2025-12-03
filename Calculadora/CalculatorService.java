84f58e7310f57873cb5da22b7cd37d2012ebe483
ALopezxd <joseamau26@gmail.com>
2025-12-03 11:29:13 -0600

    created first functions of the calculator

---
+++ Calculadora/CalculatorService.java
@@ -0,0 +1,23 @@
+package Calculadora;
+
+public class CalculatorService {
+    public double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public double restar(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+
+        return a / b;
+    }
+}
